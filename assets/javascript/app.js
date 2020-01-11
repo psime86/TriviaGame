e566675f2cc1dee736a0f2714eb0b938aeead8b7
Patrick Sime <psime86@gmail.com>
2020-01-11 00:33:32 -0600

    html and psudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    // Trivia Questions //
+    
+    // Question
+
+    // Choices
+
+    // Answer index (0-3)
+
+    // Gif/Img
+
+    // Times # of Questions //
+
+
+
+    // Variables //
+
+    // Right answers
+    // Wrong answers
+    // Skipped questions
+    // Timer
+    // User answer
+
+
+    // Start game with start button //
+    // On click event
+
+    
+    // Countdown timer //
+
+    // Timer start
+
+
+    // Picking answers //
+    // On click event
+
+    // Timer = 0 //
+    // Timer stop
+    // Time is up message
+    // Display correct answer
+    // Skip count up
+    // Show picture
+
+    // If right //
+    // Timer stop
+    // Correct message
+    // Win count up
+    // Show picture
+
+    // Else wrong //
+    // Timer stop
+    // Wrong message
+    // Display correct answer
+    // Wrong count up
+    // Show picture
+
+
+    // Game end function //
+    // Timer stop
+    // Show right/wrong/skipped count
+    // Play again?
+    // Reset game on click
+})
